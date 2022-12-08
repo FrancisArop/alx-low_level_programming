833fe7ae4a18664b727708d01f060ee095261510
francisarop <francismagood@gmail.com>
2022-12-08 12:46:04 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * 
+ * return: always 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+
+}
