fdacf174b38e006973ee74001c67dfb38b9e936e
francisarop <francismagood@gmail.com>
2022-12-13 10:09:03 -0800

    putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#ifndef HOLBERTON_H_INCLUDED
 
 /**
  * main Entry point
@@ -9,15 +8,14 @@
 
 int main(void)
 {	
-	_putchar(" ");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar('\n');
-
-	return (0);
+_putchar(" ");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar('\n');
+return (0);
 }	
