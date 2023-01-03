aba3fd1ddaf7fe61692fa31b31c774f07b7fa7ca
francisarop <francismagood@gmail.com>
2023-01-03 14:08:56 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,23 @@
 #include "main.h"
+
 /**
  * _strspn - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
  */
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int g = 0;
-	int k;
+unsigned int _strspn(char *s, char *accept) {
+    unsigned int count = 0;
 
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
-		{
-			if (*s == accept[k])
-			{
-				g++;
-				break;
-			}
-			else if (accept[k + 1] == '\0')
-				return (k);
-		}
-		s++;
-	}
-	return (g);
+    while (*s) {
+        if (strchr(accept, *s)) {
+            count++;
+            s++;
+        } else {
+            break;
+        }
+    }
+
+    return count;
 }
 
