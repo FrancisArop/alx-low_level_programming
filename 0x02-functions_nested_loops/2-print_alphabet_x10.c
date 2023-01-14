54bc6f10100de901852583013800054853d934ce
francisarop <francismagood@gmail.com>
2023-01-14 14:31:55 -0800

    2-print_alphabet_1x0

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,9 @@ for (int i = 0; i < 10; i++)
 {
 for (char c = 'a'; c <= 'z'; c++)
 {
-_putchar("%c\n", c);
+_putchar(c);
+}
+_putchar('\n');
 }
 }
-}
-int main(void)
-{
-print_alphabet_x10();
-}
+
