4fce04c5e21d2c5c0448283509583040910d89ec
francisarop <francismagood@gmail.com>
2023-01-20 12:33:15 -0800

    2-mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
 /**
- * mul - Multiplies two integers and returns the result.
- * @a: First integer to be multiplied.
- * @b: Second integer to be multiplied.
- *
- * Return: The result of the multiplication.
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
  */
 int mul(int a, int b)
 {
-if (a < 0 || b < 0)
-{
-printf("Error: Both inputs must be non-negative integers.\n");
-return (-1);
+int mul;
+
+mul = a * b;
+
+return (mul);
 }
-int result = a * b;
-return (result);
-}
+
 
